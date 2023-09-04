3752f3a8db6a71a7e060c0b961be648ceb18b0e0
Zahid <mrzahidxy@gmail.com>
2023-09-05 00:03:02 +0600

    portfolio update

--- server/routes/portfolio.js
+++ server/routes/portfolio.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const multer = require("multer");
 const Portfolio = require("../models/portfolio");
-const { ref, uploadBytes, getDownloadURL } = require("firebase/storage");
+const {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  deleteObject,
+} = require("firebase/storage");
 const storage = require("../firebase");
 
 const router = express.Router();
 const maxFileSize = 5 * 1024 * 1024; // 5MB
+
+function getObjectPathFromUrl(url) {
+  const startIndex = url.indexOf("/o/") + 3; // +3 to skip "/o/"
+  const endIndex = url.indexOf("?"); // Index of the "?" character
+
+  if (startIndex !== -1 && endIndex !== -1) {
+    const objectPath = url.substring(startIndex, endIndex);
+    return objectPath;
+  } else {
+    return null; // Invalid URL format
+  }
+}
 
 // Configure multer for file uploads
 const upload = multer({
@@ -49,6 +66,70 @@ router.post("/", upload.single("image"), async (req, res) => {
     res.status(201).json(createdPortfolio);
   } catch (error) {
     console.error("Error creating portfolio item:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Update an existing portfolio item
+router.put("/:id", upload.single("image"), async (req, res) => {
+  try {
+    const { title, description, link } = req.body;
+    const img = req.file;
+    const portfolioItemId = req.params.id;
+
+    // First, delete the old image from Firebase Storage (if it exists)
+    const existingPortfolioItem = await Portfolio.findById(portfolioItemId);
+    if (existingPortfolioItem) {
+      // Extract the filename from the existing image URL
+      const existingImageRef = ref(
+        storage,
+        getObjectPathFromUrl(existingPortfolioItem.img)
+      );
+
+      // Delete the existing image
+      try {
+        await deleteObject(existingImageRef);
+      } catch (error) {
+        console.error("Error deleting existing image:", error);
+      }
+    }
+
+    // Upload the new image to Firebase Storage
+    if (img) {
+      const filename = `${Date.now()}_${img.originalname}`;
+      const fileRef = ref(storage, filename);
+      await uploadBytes(fileRef, img.buffer);
+      const imageUrl = await getDownloadURL(fileRef);
+
+      // Update the portfolio item in the database with the new image URL
+      const updatedPortfolioItem = await Portfolio.findByIdAndUpdate(
+        portfolioItemId,
+        {
+          title,
+          description,
+          img: imageUrl,
+          link,
+        },
+        { new: true }
+      );
+
+      res.status(200).json(updatedPortfolioItem);
+    } else {
+      // If no new image is provided, update the portfolio item without changing the image
+      const updatedPortfolioItem = await Portfolio.findByIdAndUpdate(
+        portfolioItemId,
+        {
+          title,
+          description,
+          link,
+        },
+        { new: true }
+      );
+
+      res.status(200).json(updatedPortfolioItem);
+    }
+  } catch (error) {
+    console.error("Error updating portfolio item:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
